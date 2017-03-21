eba6ba94df7fb3ed1c216fe4c797bb2e6a5d4f5a
Joel Klint <joel.klint@gmail.com>
2017-03-21 15:33:16 +0100

    Made tables for user, hashtags and user_relations

--- set_up_database.sql
+++ set_up_database.sql
@@ -11,4 +11,32 @@ CREATE TABLE IF NOT EXISTS tweets (
     lang
 )
 
-hashtags 
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT,
+    user_screen_name TEXT,
+    user_name TEXT,
+    user_location TEXT,
+    user_description TEXT,
+    user_followers_count INT, 
+    user_friends_count INT,
+    user_time_zone TEXT,
+    user_lang TEXT,
+    user_url TEXT,
+    user_geo_enabled BOOLEAN,
+    PRIMARY KEY(user_id)
+)
+
+CREATE TABLE IF NOT EXISTS user_relations (
+    follower_user_id TEXT,
+    following_user_id TEXT,
+    PRIMARY KEY(follower_user_id, following_user_id),
+    FOREIGN KEY(follower_user_id) REFERENCES users(user_id),
+    FOREIGN KEY(following_user_id) REFERENCES users(user_id),
+)
+
+CREATE TABLE IF NOT EXISTS tweet_hashtags (
+    tweet_id INT,
+    hashtag TEXT,
+    PRIMARY KEY(tweet_id, hashtag),
+    FOREIGN KEY(tweet_id) tweets(id)
+)
