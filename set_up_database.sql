10f5f9a352f56c5002c20b7f32667ecd73979dab
Oscar Rydh <oscar.rydh.93@gmail.com>
2017-03-21 15:29:44 +0100

    Created the tweets table schema

--- set_up_database.sql
+++ set_up_database.sql
@@ -2,13 +2,18 @@ CREATE TABLE IF NOT EXISTS tweets (
     id INT,
     text TEXT,
     geo TEXT,
-    coordinates 
-    place
-    retweeted_id
-    original_tweet_retweet_count
-    in_reply_to_status_id
-    in_reply_to_user_id
-    lang
+    user_id INT,
+    tweet_hashtag_id,
+    longitude FLOAT,
+    latitude FLOAT,
+    place_id INT,
+    retweeted_id INT,
+    original_tweet_retweet_count INT,
+    in_reply_to_status_id INT,
+    in_reply_to_user_id INT,
+    lang TEXT
+    PRIMARY KEY (id)
+    FOREIGN KEY (place_id) REFERENCES places(place_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (retweeted_id) REFERENCES tweets(retweeted_id)
 )
-
-hashtags 
