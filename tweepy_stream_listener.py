2241c65f00f0609fefcb79d8aa27d47216fb645f
Oscar Rydh <oscar.rydh.93@gmail.com>
2017-03-21 16:01:35 +0100

    Now our stream listner also gets place

--- tweepy_stream_listener.py
+++ tweepy_stream_listener.py
@@ -23,7 +23,40 @@ class StreamListener(tweepy.StreamListener):
             coordinates = coordinates.get('coordinates')
             longitude = coordinates[0]
             latitude = coordinates[1]
+
+        #Handle place data
         place = data.get('place')
+        if place != None:
+            print(place)
+            place_name = place.get('name', None)
+            place_country = place.get('country', None)
+            place_contry_code = place.get('country_code', None)
+            place_url = place.get('url', None)
+            place_id = place.get('id', None)
+            place_full_name = place.get('full_name', None)
+            place_type = place.get('place_type', None)
+
+            attributes = place.get('attributes', None)
+            if attributes != None:
+                attribute_street_address = attributes.get('street_address', None)
+                attribute_locality = attributes.get('locality', None)
+                attribute_region = attributes.get('region', None)
+                attribute_isothree = attributes.get('iso3', None)
+                attribute_postal_code = attributes.get('postal_code', None)
+
+            bounding_box = place.get('bounding_box', None)
+            print (bounding_box)
+            if bounding_box != None:
+                bound_coordinates = bounding_box.get('coordinates', None)
+                bound_type = bounding_box.get('type', None)
+                if coordinates != None:
+                    for coordinate_middleware in coordinates:
+                        for coordinate in coordinate_middleware:
+                            # INSERT INTO BOUNDING BOX TABLE HERE
+                            bound_longitude = coordinate[0]
+                            bound_latitude = coordinate[1]
+
+
         #Handle retweets
         retweeted_id = data.get('retweeted_status', {}).get('id')
         if retweeted_id != None:
@@ -33,7 +66,6 @@ class StreamListener(tweepy.StreamListener):
         in_reply_to_status_id = data.get('in_reply_to_status_id')
         in_reply_to_user_id = data.get('in_reply_to_user_id')
         lang = data.get('lang')
-        print (retweeted_id)
         if latitude != None:
             print(longitude)
             print(latitude)
