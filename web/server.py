5c5b968641e184ab852e2cfd493925f895329f59
oscar-rydh <oscar.rydh.93@gmail.com>
2017-03-28 18:43:07 +0200

    Satte upp en början på en web

---
+++ web/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from database import Database
+app = Flask(__name__)
+
+
+DB_NAME = 'twitter-geo'
+
+print('Connection Completed')
+
+@app.route("/", methods=['GET'])
+def index():
+    db = Database(DB_NAME)
+    nbrOfUsers = db.selectNumberOfUsers()
+    return render_template('base.html', nbrOfUsers=nbrOfUsers)
+
+@app.route("/search/", methods=['GET'])
+@app.route("/search/<table>", methods=['GET'])
+def searchTemplate(table=None):
+    db = Database(DB_NAME)
+    tables = db.selectAllTables()
+    if (table):
+        data = db.generateTableData(table)
+    return render_template('search.html', tables=tables, data=data)
+
+
+
+
+app.run(debug=True)
