98e7d1a5641b09ae110f516fce7836247cc7891c
Oscar Rydh <oscar.rydh.93@gmail.com>
2017-04-10 16:44:19 +0200

    Fixed mayor bug

--- place_with_contains.py
+++ place_with_contains.py
@@ -18,12 +18,14 @@ for user_location in user_locations:
     print ("Parsing: ", user_location)
     max = 0
     max_geoid = 0
+    has_been_inserted = False
     for db_location in db_locations:
         #if not(db_location[0] == None) and user_location in db_location[0]:
         if not(db_location[1] == None):
             ratio = similar(user_location[1], db_location[1])
             if ratio == 1:
                 db.set_filtered_location(user_location[0], db_location[0], ratio)
+                has_been_inserted = True
                 break
             elif ratio > max:
                 max = ratio
@@ -34,12 +36,13 @@ for user_location in user_locations:
             ratio = similar(user_location[1], db_location[2])
             if ratio == 1:
                 db.set_filtered_location(user_location[0], db_location[0], ratio)
+                has_been_inserted = True
                 break
             elif ratio > max:
                 max = ratio
                 max_geoid = db_location[0]
 
-    if (max_geoid != 0):
+    if (max_geoid != 0 and not(has_been_inserted)):
         db.set_filtered_location(user_location[0], max_geoid, max)
 
 print ("WE ARE DONE")
