3dae4b07911ff1e4ceba6c97b448cb95f2dc273d
oscar-rydh <oscar.rydh.93@gmail.com>
2017-04-11 13:25:10 +0200

    Completed the preprocess script

--- preprocess_user_location_in_db.py
+++ preprocess_user_location_in_db.py
@@ -18,7 +18,30 @@ def get_all_users_with_location():
 
     return result_tuple
 
+def insert_into_preprocessed(location, user_id, rest=None):
+    cur = conn.cursor()
+    if rest != None:
+        statement = """
+            UPDATE users
+            SET preprocessed_location = %s,
+                preprocessed_rest = %s
+            WHERE user_id = %s
+        """
+        cur.execute(statement, (location, rest, user_id))
+    else:
+        statement = """
+            UPDATE users
+            SET preprocessed_location = %s,
+                preprocessed_rest = DEFAULT
+            WHERE user_id = %s
+        """
+        cur.execute(statement, (location, user_id))
+    conn.commit()
+    cur.close()
+
 all_users = get_all_users_with_location()
+
+i = 0
 
 for user in all_users:
     index = user[1].find(",")
@@ -26,6 +49,24 @@ for user in all_users:
         location = user[1][0:index]
         rest = user[1][index:]
         user = (user[0],) + (location, rest,)
+        rest_split = user[2].split(',')
+        if (len(rest_split) > 1 and len(rest_split) < 3 and not(rest_split[1] == "") and rest_split[1][0] == " "):
+            rest = rest_split[1][1:]
+        else:
+            rest = ",".join(rest_split)
+        insert_into_preprocessed(location, user[0], rest)
+    else:
+        insert_into_preprocessed(user[1], user[0])
+    i+= 1
+
+    if (i%10000 == 0):
+        print('We have completed: ', i)
+    #print(location)
+    #print (rest)
+    #print(user[0])
+    #
+
+
 
     # Save in DB
     # ALTER TABLE IN DB
