ff10f552643654c62a59c8091e29958473257be2
Joel Klint <joel.klint@gmail.com>
2017-04-24 16:38:29 +0200

    Can now fetch a bounding box for a tweet

--- layers_method/places-twitter.py
+++ layers_method/places-twitter.py
@@ -1,15 +1,24 @@
 import psycopg2
 
-def get_place_bounding_boxes_from_db():
+# Returns a bounding box for a tweet
+def get_place_bounding_boxes_for_tweet(tweet_id):
     conn = psycopg2.connect('dbname={}'.format('twitter-geo'))
     cur = conn.cursor()
     statement = """
-        SELECT min(longitude), min(latitude), max(longitude), max(latitude) 
-        FROM place_bounding_box_coordinate 
+        SELECT min(p.longitude), min(p.latitude), max(p.longitude), max(p.latitude) 
+        FROM place_bounding_box_coordinate as p
+        INNER JOIN tweets
+        USING(place_id)
+        WHERE tweets.id = %s
+        AND tweets.place_id IS NOT NULL
         GROUP BY place_id
     """
-    cur.execute(statement)
+    cur.execute(statement, (tweet_id,))
     conn.commit()
     bounding_boxes = cur.fetchall()
     cur.close()
-    return bounding_boxes
+    result = ()
+    if len(bounding_boxes) == 0: return None
+    for val in bounding_boxes[0]:
+        result = (float(val),) + result
+    return result
