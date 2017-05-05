3e0e820f29b32adf395d8f5f21ef25da6a03347d
Joel Klint <joel.klint@gmail.com>
2017-05-05 13:09:51 +0200

    Made front end map render locations provided by backend

--- web/server.py
+++ web/server.py
@@ -45,12 +45,6 @@ def searchTemplate(table='users'):
         data = db.generateTableData(table)
     return render_template('search.html', tables=tables, data=data)
 
-
-@app.route("/test")
-def test():
-    db = Database(DB_NAME)
-    db.nbrOfUsersWithLocation()
-
 @app.route("/cluster_data", methods=['GET'])
 def cluster():
     return render_template('cluster_data.html')
@@ -66,6 +60,43 @@ def get_data():
         list_data.append([lat, long])
     result = list_data
     return jsonify(result)
+
+# This is just a mock. Fill it will real data
+@app.route('/locations', methods=['GET'])
+def get_locations():
+    locations = [
+        {'lat': -31.563910, 'lng': 147.154312},
+        {'lat': -33.718234, 'lng': 150.363181},
+        {'lat': -33.727111, 'lng': 150.371124},
+        {'lat': -33.848588, 'lng': 151.209834},
+        {'lat': -33.851702, 'lng': 151.216968},
+        {'lat': -34.671264, 'lng': 150.863657},
+        {'lat': -35.304724, 'lng': 148.662905},
+        {'lat': -36.817685, 'lng': 175.699196},
+        {'lat': -36.828611, 'lng': 175.790222},
+        {'lat': -37.750000, 'lng': 145.116667},
+        {'lat': -37.759859, 'lng': 145.128708},
+        {'lat': -37.765015, 'lng': 145.133858},
+        {'lat': -37.770104, 'lng': 145.143299},
+        {'lat': -37.773700, 'lng': 145.145187},
+        {'lat': -37.774785, 'lng': 145.137978},
+        {'lat': -37.819616, 'lng': 144.968119},
+        {'lat': -38.330766, 'lng': 144.695692},
+        {'lat': -39.927193, 'lng': 175.053218},
+        {'lat': -41.330162, 'lng': 174.865694},
+        {'lat': -42.734358, 'lng': 147.439506},
+        {'lat': -42.734358, 'lng': 147.501315},
+        {'lat': -42.735258, 'lng': 147.438000},
+        {'lat': -43.999792, 'lng': 170.463352}
+    ]
+    return jsonify(locations)
+
+    # {
+    #     user_id: user_id,
+    #     lat: predicted_lat,
+    #     lng: predicted_long,
+    #     value: max_value,
+    # }
 
 
 app.run(debug=True)
